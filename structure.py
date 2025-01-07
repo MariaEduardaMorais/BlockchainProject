d8005e0da4958e2df5e5ab725bb1f5dd354d2e03
Maria Eduarda <meramorais12@gmail.com>
2025-01-07 20:26:22 -0300

    Updating code

--- structure.py
+++ structure.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import requests
 from time import time
 from flask import Flask
 
@@ -219,6 +220,50 @@ class Blockchain:
         }
         return jsonify(response), 200
     
+    @app.route('/nodes/register', methods=['POST'])
+    def register_nodes():
+        values = request.get_json()
+
+        nodes = values.get('nodes')
+        if nodes is None:
+            return "Error: Please supply a valid list of nodes", 400
+
+        for node in nodes:
+            blockchain.register_node(node)
+
+        response = {
+            'message': 'New nodes have been added',
+            'total_nodes': list(blockchain.nodes),
+        }
+        return jsonify(response), 201
+
+    @app.route('/nodes/resolve', methods=['GET'])
+    def consensus():
+        replaced = blockchain.resolve_conflicts()
+
+        if replaced:
+            response = {
+                'message': 'Our chain was replaced',
+                'new_chain': blockchain.chain
+            }
+        else:
+            response = {
+                'message': 'Our chain is authoritative',
+                'chain': blockchain.chain
+            }
+
+        return jsonify(response), 200
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
+
+# Create a new transaction
+new_transaction = {
+    'sender': 'Alice',
+    'recipient': 'Bob',
+    'amount': 5,
+}
+
+# Send the transaction to the server
+response = requests.post('http://localhost:5000/transactions/new', json=new_transaction)
+print(response.json())
